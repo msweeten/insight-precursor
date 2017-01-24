1301f3af4b5302ba3d581537e4cb89d0039d49cb
msweeten <msweeten43@gmail.com>
2017-01-24 09:38:30 -0500

    Initialized Python scripts

---
+++ InitialDataClean.py
@@ -0,0 +1,18 @@
+def load_data_csv():
+    """If no database, creates PostgreSQL database from csv
+    """
+    pass
+
+def load_data_sql():
+    """Loads data from SQL into appropriate network format
+    """
+    pass
+
+def match_songs():
+    """Checks SQL database for song_node variable
+       Else initializes song_node variable
+    """
+    pass
+
+if __name__ == '__main__':
+    pass
---
+++ NetworkAnalysis.py
@@ -0,0 +1,24 @@
+def implement_network():
+    """Implements a network for semi-supervised learning
+    """
+    pass
+
+def learning_algorithm():
+    """Defines the learning algorithm and applies to data
+    """
+    #May have to call wvRN and Java
+    pass
+
+def identify_same_songs():
+    """Identifies likely same songs
+    """
+    pass
+
+def load_db():
+    """Pulls relational data from network 
+       or creates database if no database
+    """
+    pass
+
+if __name__ == '__main__':
+    pass
