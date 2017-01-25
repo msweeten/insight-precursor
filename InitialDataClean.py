6fc11878cea3fcd4f8c3f2f446c4bb276e9d204d
msweeten <msweeten43@gmail.com>
2017-01-25 11:59:10 -0500

    Match Songs for Nodes

--- InitialDataClean.py
+++ InitialDataClean.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine
+import itertools
 
 def load_data_sql():
     """Loads data from SQL
@@ -12,9 +13,10 @@ def load_data_sql():
     pswd = split[3]
     con = None
     con = psycopg2.connect(database = dbname, user = username, host='localhost', password=pswd)
-    return(con)
+    engine = create_engine('postgresql://%s:%s@localhost/%s'%(username,pswd,dbname))    
+    return(con, engine)
 
-def match_songs(con):
+def match_songs(con, engine):
     """Checks SQL database for song_node variable
        Else initializes song_node variable
     """
@@ -56,12 +58,27 @@ def match_songs(con):
     else:
         pass
 
-def create_network():
-    """Implements a network for semi-supervised learning
+def create_network(con, engine):
+    """Create the network and places into SQL table in spot_db
     """
-    pass
+    network = pd.DataFrame(columns = ('Node A', 'Node B', 'Weight'))
+    query = 'SELECT * FROM song_data_table_nodes;'
+    dataset = pd.read_sql_query(query, con)
+    albums = list(set(dataset['album_uri'].values))
+    for a in albums:
+        album_sub = dataset[dataset['album_uri'] == a]
+        nodes = list(set(album_sub['nodes'].values))
+        nodes.sort()
+        edges = list(itertools.combinations(nodes, 2))
+        for e in edges:
+            edge_list = list(e) + [1]
+            network.append(edge_list, ignore_index = True)
 
+    network['Node A'] = 'Node ' + network['Node A'].astype(str)
+    network['Node B'] = 'Node ' + network['Node B'].astype(str)    
+    network.to_sql('network', engine, if_exists='replace')
 if __name__ == '__main__':
-    con = load_data_sql()
-    match_songs(con)
+    con, engine = load_data_sql()
+    match_songs(con, engine)
+    create_network(con,engine)
 
