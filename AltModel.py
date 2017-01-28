842ad716bc6302c1ba66159e7cff061fd819ce75
msweeten <msweeten43@gmail.com>
2017-01-28 18:46:05 -0500

    Modified model and updated ignore

--- AltModel.py
+++ AltModel.py
@@ -63,43 +63,11 @@ def describe_communities(network, con):
         node_length = count[v]
         weights.append(node_length)
     info_map = network.community_infomap()
-    info_map50 = network.community_infomap(trials = 50)
+    info_map50 = network.community_infomap(weights, trials = 50)
     with open('communities.pickle', 'rb') as f:
         pickle.dump(infomap50, f)
     return(info_map, info_map50)
             
-    
-
-def assign_labels(model, genres, con, engine):
-    """Assigns labels to communities via 
-    Lists the top 3 weighted labels (based on training set) 
-    For each community
-
-    Places data in SQL database
-
-    Model is an iterable datatype
-    """
-    query = 'SELECT * FROM node_data WHERE Known = 1;'
-    db_nodes = pd.read_sql_query(query, con)
-    music_results = pd.DataFrame(columns = ('Node', 'Community', 'Label1', 'Label2', 'Label3'))
-    labels = []
-    node_iter = 0
-    for nodes in model:
-        node_list = list(nodes)
-
-        node_genres = db_nodes['Genre'][db_nodes['Node'].isin(node_list)]
-        node_genres = node_genres.values
-        counts = Counter(node_genres)
-        most_common = counts.most_common(3)
-        labels = ['']*3
-        for m in range(len(most_common)):
-            curr_label = list(m)[0]
-            labels[m] = curr_label
-        for n in node_list:
-            node_data = [n, node_iter] + labels
-            music_results.append(node_data)
-        node_iter += 1
-    music_results.to_sql('communities', engine, if_exists = 'replace')
 if __name__ == '__main__':
     print('Starting...')
     dataset, genres, con, engine = load_network_db()
